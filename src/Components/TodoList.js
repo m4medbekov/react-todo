a0cd22fdeb2775077bad97f48b7b5764e916fd61
m4medbekov <m4medbekov@gmail.com>
2022-05-11 21:22:07 +0300

    added an object with styles (one of the variations of the styles of this project)

--- src/Components/TodoList.js
+++ src/Components/TodoList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import TodoItem from "./TodoItem";
+
+const styles = {
+    ul: {
+        listStyle: 'none',
+        margin: 0,
+        padding: 0
+    }
+}
 
 export default function TodoList() {
     return (
-        <ul>
-            <li>1</li>
-            <li>2</li>
+        <ul style={styles.ul}>
+            <TodoItem />
+            <TodoItem />
+            <TodoItem />
         </ul>
     )
 }
