c95f5e2dd6cd622008041c32dad63b1e657c5aa2
m4medbekov <m4medbekov@gmail.com>
2022-05-12 20:05:28 +0300

    Create a state to refresh completed status by clicking on checkbox

--- src/App.js
+++ src/App.js
@@ -1,20 +1,23 @@
+import { useState } from "react";
 import TodoList from "./Components/TodoList";
 
 function App() {
 
-  let arrayOfTodoItems = [
+  let [arrayOfTodoItems, setArrayOfTodoItems] = useState([
     {id: 1, completed: false, title: 'Купить хлеб'},
     {id: 2, completed: false, title: 'Купить молоко'},
     {id: 3, completed: false, title: 'Купить масло'}
-  ]
+  ])
 
   function onToggle(id) {
-    arrayOfTodoItems.map(item => {
-      if (item.id === id) {
-        item.completed = !item.completed
-      }
-      return item
-    })
+    setArrayOfTodoItems(
+      arrayOfTodoItems.map(item => {
+        if (item.id === id) {
+          item.completed = !item.completed
+        }
+        return item
+      })
+    )
   }
   
   return (
