9fd875f79dab68f2f369dfdf91c489eaca884f9b
m4medbekov <m4medbekov@gmail.com>
2022-05-12 22:00:50 +0300

    Make custom hook for input (include 3 func for work with input)

--- src/Components/AddItem.js
+++ src/Components/AddItem.js
@@ -1,22 +1,34 @@
 import React, {useState} from "react";
 import PropTypes from "prop-types";
 
+function useInputValue(defaultValue = '') {
+    const [value, setValue] = useState(defaultValue)
+     
+    return {
+        bind: {
+            value,
+            onChange: event => setValue(event.target.value)
+        },
+        clear: () => setValue(''),
+        value: () => value   
+    }
+}
+
 function AddItem({onCreate}) {
-    const [value, setValue] = useState('')
+    const input = useInputValue('')
 
     function submitHandler(event) {
         event.preventDefault()
 
-        if (value.trim()) {
-            onCreate(value)
+        if (input.value().trim()) {
+            onCreate(input.value())
+            input.clear()
         }
-        
-        setValue('')
     }
 
     return (
         <form style={{marginBottom: '1rem'}} onSubmit={submitHandler}>
-            <input value={value} onChange={event => setValue(event.target.value)} />
+            <input {...input.bind} />
             <button type="button">Добавить задачу</button>
         </form>
     )
