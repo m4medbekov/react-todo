7348e9d92f3b08961742ce0045d750e35f00118a
m4medbekov <m4medbekov@gmail.com>
2022-05-12 19:24:43 +0300

    Add another type of styles in react

--- src/Components/TodoItem.js
+++ src/Components/TodoItem.js
@@ -1,11 +1,31 @@
 import React from "react";
 import PropTypes from "prop-types";
 
+const sytles = {
+    li: {
+        display: 'flex',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        padding: '.5rem 1rem',
+        border: '1px solid #ccc',
+        borderRadius: '4px',
+        marginBottom: '.5rem'
+    },
+    input: {
+        marginRight: '1rem'
+    }
+}
+
 function TodoItem({item, index}) {
     return (
-        <li>
-            <strong>{index + 1}</strong>
-            {item.title}
+        <li style={sytles.li}>
+            <span>
+                <input type='checkbox' style={sytles.input} />
+                <strong>{index + 1}</strong>
+                &nbsp;
+                {item.title}
+            </span>
+            <button className="removeBtn">&times;</button>
         </li>
     )
 }
